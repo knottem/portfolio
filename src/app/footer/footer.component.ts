d2c9421cb506019e2f967cf4116e6c513518001b
Erik Wallenius <walle.o.w@gmail.com>
2025-01-13 08:08:11 +0100

    added the footer component

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatAnchor} from '@angular/material/button';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
