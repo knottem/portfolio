3c10b8bc1c884605b8b2cb3a0f252658ea13f987
Erik Wallenius <walle.o.w@gmail.com>
2025-01-15 01:19:32 +0100

    fixed links not really showing in the translation texts

--- src/app/shared/scroll-in-view.directive.spec.ts
+++ src/app/shared/scroll-in-view.directive.spec.ts
@@ -1,8 +1,21 @@
 import { ScrollInViewDirective } from './scroll-in-view.directive';
+import { ElementRef, Renderer2 } from '@angular/core';
 
 describe('ScrollInViewDirective', () => {
+  let directive: ScrollInViewDirective;
+  let mockElementRef: ElementRef;
+  let mockRenderer: Renderer2;
+
+  beforeEach(() => {
+    // Mock dependencies
+    mockElementRef = { nativeElement: {} } as ElementRef;
+    mockRenderer = jasmine.createSpyObj('Renderer2', ['addClass']);
+
+    // Create the directive instance
+    directive = new ScrollInViewDirective(mockElementRef, mockRenderer);
+  });
+
   it('should create an instance', () => {
-    const directive = new ScrollInViewDirective();
     expect(directive).toBeTruthy();
   });
 });
