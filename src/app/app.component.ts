8e8ea7a4358859cfb37af36fd1abe7d1d17b46fe
Erik Wallenius <walle.o.w@gmail.com>
2025-02-03 14:28:33 +0100

    removed unused method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,32 +28,4 @@ export class AppComponent {
     this.sharedService.initializeLanguage();
   }
 
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const sections = document.querySelectorAll('section');
-    let activeSection: string | null = null;
-    let maxVisibleArea = 0;
-
-    sections.forEach((section, index) => {
-      const rect = section.getBoundingClientRect();
-      const visibleHeight = Math.min(rect.bottom, window.innerHeight) - Math.max(rect.top, 0);
-      if (
-        index === sections.length - 1 &&
-        rect.top >= 0 &&
-        rect.bottom <= window.innerHeight
-      ) {
-        activeSection = section.id;
-        return;
-      }
-
-      if (visibleHeight > 0 && visibleHeight > maxVisibleArea) {
-        maxVisibleArea = visibleHeight;
-        activeSection = section.id;
-      }
-    });
-
-    if (activeSection) {
-      this.sharedService.setActiveSection(activeSection);
-    }
-  }
 }
